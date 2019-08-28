34b63903fbbaf097443a785d17ef99a0914e50c1
Sarah Drake <jussaradrake@outlook.com>
2019-08-28 13:46:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,15 @@
 class Dog
-   def initialize(dogs_name)
-    @name = dogs_name
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
  
-  def dogs_name
-    @name
+  def name
+    @this_dogs_name
   end
 end
  
-fido = Dog.new("fido")
-fido.dogs_name
+fido = Dog.new
+fido.name = "fido"
+ 
+puts fido.name
